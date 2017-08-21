354ffef38596abad2b213ebafadee2a86b152111
K0SiK <luk.kosek@seznam.cz>
2017-08-21 21:00:31 +0200

    Alfa

--- Assets/ScalableCamera.cs
+++ Assets/ScalableCamera.cs
@@ -5,29 +5,48 @@ public class ScalableCamera : MonoBehaviour
 {
 
     // Use this for initialization
+    // Use this for initialization
     void Start()
     {
-        float TARGET_WIDTH = 960.0f;
-        float TARGET_HEIGHT = 540.0f;
-        int PIXELS_TO_UNITS = 30; // 1:1 ratio of pixels to units
+        // set the desired aspect ratio (the values in this example are
+        // hard-coded for 16:9, but you could make them into public
+        // variables instead so you can set them at design time)
+        float targetaspect = 19.0f / 9.0f;
 
-        float desiredRatio = TARGET_WIDTH / TARGET_HEIGHT;
-        float currentRatio = (float)Screen.width / (float)Screen.height;
+        // determine the game window's current aspect ratio
+        float windowaspect = (float)Screen.width / (float)Screen.height;
 
-        if (currentRatio >= desiredRatio)
+        // current viewport height should be scaled by this amount
+        float scaleheight = windowaspect / targetaspect;
+
+        // obtain camera component so we can modify its viewport
+        Camera camera = GetComponent<Camera>();
+
+        // if scaled height is less than current height, add letterbox
+        if (scaleheight < 1.0f)
         {
-            // Our resolution has plenty of width, so we just need to use the height to determine the camera size
-            Camera.main.orthographicSize = TARGET_HEIGHT / 4 / PIXELS_TO_UNITS;
-        }
-        else
-        {
-            // Our camera needs to zoom out further than just fitting in the height of the image.
-            // Determine how much bigger it needs to be, then apply that to our original algorithm.
-            float differenceInSize = desiredRatio / currentRatio;
-            Camera.main.orthographicSize = TARGET_HEIGHT / 4 / PIXELS_TO_UNITS * differenceInSize;
-        }
+            Rect rect = camera.rect;
 
-       
+            rect.width = 1.0f;
+            rect.height = scaleheight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleheight) / 2.0f;
+
+            camera.rect = rect;
+        }
+        else // add pillarbox
+        {
+            float scalewidth = 1.0f / scaleheight;
+
+            Rect rect = camera.rect;
+
+            rect.width = scalewidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scalewidth) / 2.0f;
+            rect.y = 0;
+
+            camera.rect = rect;
+        }
     }
 
 }
